0c676ff31fd8de00022932995a18cd65fbbfb0fe
Katasuki <daravuthwatna@gmail.com>
2024-08-18 13:33:00 +0700

    frontend

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
 import React, { useContext, useState, useEffect } from "react";
 import { ProductContext } from "../context/ProductContext";
